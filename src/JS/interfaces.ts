4955981026da9a604e4ec05cfd8981762805f8e5
ralph1786 <ralph1786@gmail.com>
2020-01-14 10:10:22 -0500

    created interface file

---
+++ src/JS/interfaces.ts
@@ -0,0 +1,14 @@
+export interface SpotInformation {
+  id: string;
+  review: string;
+  address: string;
+  name: string;
+}
+
+export interface Spot {
+  id: number;
+  image: string;
+  address: string;
+  review: string;
+  name: string;
+}
