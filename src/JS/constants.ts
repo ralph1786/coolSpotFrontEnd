5c79ebf8c2fd84d02f0d1ba8954f1ae13f0a49c9
ralph1786 <ralph1786@gmail.com>
2020-01-14 10:09:50 -0500

    changed file from js to ts, added type casting

---
+++ src/JS/constants.ts
@@ -0,0 +1,28 @@
+export const apiUrlLocations = "http://localhost:3000/api/v1/locations";
+export const apiUrlSpots = "http://localhost:3000/api/v1/spots";
+export const locationsContainer = document.querySelector(
+  ".list-locations"
+) as HTMLUListElement;
+export const spotsContainer = document.querySelector("#main") as HTMLDivElement;
+export const locationButton = document.querySelector(
+  ".location-button"
+) as HTMLButtonElement;
+export const formContainer = document.querySelector(
+  ".form-container"
+) as HTMLDivElement;
+export const formContent = document.querySelector(
+  ".form-content"
+) as HTMLDivElement;
+export const createNewSpotButton = document.querySelector(
+  ".create-new-spot-button"
+) as HTMLButtonElement;
+export const createFormContainer = document.querySelector(
+  ".create-form-container"
+) as HTMLDivElement;
+export const closeButton = document.querySelector(
+  ".close-btn"
+) as HTMLButtonElement;
+
+export const form = document.querySelector(".create-form") as HTMLFormElement;
+
+export const modal = document.getElementById("myModal") as HTMLDivElement;
