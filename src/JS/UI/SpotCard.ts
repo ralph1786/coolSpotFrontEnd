797bba8bc436c4f0410336127fcd7c1f9f46dc0a
ralph1786 <ralph1786@gmail.com>
2020-01-14 10:11:14 -0500

    changed to ts file, added interface

---
+++ src/JS/UI/SpotCard.ts
@@ -0,0 +1,17 @@
+import { Spot } from "../interfaces";
+
+export const templateSpotCard = <T extends Spot>(spot: T) => {
+  // console.log(spot);
+  return `
+    <div class="card animated fadeIn" data-spot-id="${spot.id}">
+    <img src="${spot.image}" alt="Avatar" class="image" data-spot-id="${spot.id}">
+    <div class="container" data-spot-id="${spot.id}">
+        <h4 data-spot-id="${spot.id}" class="name">${spot.name}</h4> 
+        <p data-spot-id="${spot.id}" class="address">${spot.address}</p>
+        <p class="review" data-spot-id="${spot.id}">${spot.review}</p>
+        <button class="js-edit" data-spot-id="${spot.id}">Edit</button>
+        <button class="js-delete" data-spot-id="${spot.id}">Delete</button> 
+    </div>
+    </div>
+    `;
+};
