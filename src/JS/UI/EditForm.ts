1095f98cd5be798657fab32101a01086cf11c10a
ralph1786 <ralph1786@gmail.com>
2020-01-16 13:09:46 -0500

    added label and cancel button

--- src/JS/UI/EditForm.ts
+++ src/JS/UI/EditForm.ts
@@ -1,14 +1,16 @@
 import { SpotInformation } from "../interfaces";
+import "../../form.css";
 
 export const editFormTemplate = (spotInfo: SpotInformation) => {
   return `
   <form data-spot-id="${spotInfo.id}" class="edit-form">
       <div>
-      <h4>Edit Your Review</h4>
-      <label>Review</label>
+      <h3>Edit Your Review</h3>
+      <label for="review">Review:</label>
       <input type="text" id="quote-edit" value="${spotInfo.review}" name="review">
       </div>
       <button type="submit" class="edit-submit">Submit</button>
   </form>
+  <button class="edit-cancel">Cancel</button>
   `;
 };
