61e6349e4980e39eb07447bff7d421b8172b3663
ralph1786 <ralph1786@gmail.com>
2020-01-17 15:32:54 -0500

    added dynamic options

--- src/JS/utils/allLocations.ts
+++ src/JS/utils/allLocations.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 import {
   apiUrlLocations,
   spotsContainer,
-  locationsContainer
+  locationsContainer,
+  locationOptions
 } from "../constants";
 
 import { Location } from "../interfaces";
@@ -16,11 +17,18 @@ function fetchAllLocations() {
       h3.appendChild(document.createTextNode("Please Choose A Location"));
       spotsContainer.appendChild(h3);
       locations.data.forEach((location: Location) => {
+        // code below adds all locations to sideNav
         const li = document.createElement("li");
         li.classList.add("location");
         li.setAttribute("data-location-id", `${location.id}`);
         li.appendChild(document.createTextNode(location.name));
         locationsContainer.appendChild(li);
+
+        //code below appends options to the select menu in the form to create new spots.
+        const option = document.createElement("option");
+        option.setAttribute("value", `${location.id}`);
+        option.appendChild(document.createTextNode(location.name));
+        locationOptions.appendChild(option);
       });
     })
     .catch(err => console.log(err));
